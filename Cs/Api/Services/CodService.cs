c66113c8b4757605398bb6bc368b7382253c58f0
FrankLovelace <alexandrogallegos12@gmail.com>
2026-03-12 10:37:18 -0600

    Update sql and we added some variables, i may have to make them nullable

--- Cs/Api/Services/CodService.cs
+++ Cs/Api/Services/CodService.cs
@@ -1,4 +1,5 @@
 using MySqlConnector;
+using Api.Models;
 
 namespace Api.Services;
 
@@ -7,17 +8,13 @@ public class CodService
     private readonly string _connectionString = 
         "Server=sql.crystallography.net;Port=3306;Database=cod;Uid=cod_reader;Pwd=;";
 
-    public async Task<List<string>> SearchByFormulaAsync(string formula)
+   public async Task<List<CodResult>> SearchByFormulaAsync(string formula)
 {
-    var results = new List<string>();
+    var results = new List<CodResult>();
     using var connection = new MySqlConnection(_connectionString);
     await connection.OpenAsync();
 
-    string sql = @"SELECT file, formula, commonname, year 
-                   FROM data 
-                   WHERE formula LIKE @formula OR commonname LIKE @formula 
-                   ORDER BY year DESC 
-                   LIMIT 10";
+    string sql = "SELECT file, formula, commonname, year, a, b, c, alpha, beta, gamma FROM data WHERE formula LIKE @formula LIMIT 10";
 
     using var command = new MySqlCommand(sql, connection);
     command.Parameters.AddWithValue("@formula", $"%{formula}%");
@@ -25,13 +22,22 @@ public class CodService
     using var reader = await command.ExecuteReaderAsync();
     while (await reader.ReadAsync())
     {
-        var id = reader.GetInt32("file");
-        var f = reader.IsDBNull(reader.GetOrdinal("formula")) ? "?" : reader.GetString("formula");
-        var n = reader.IsDBNull(reader.GetOrdinal("commonname")) ? "Mineral" : reader.GetString("commonname");
-        var y = reader.IsDBNull(reader.GetOrdinal("year")) ? "" : reader.GetInt32("year").ToString();
-        
-        results.Add($"[COD {id}] {f} - {n} ({y})");
+        results.Add(new CodResult 
+        {
+            Id = reader.GetInt32(reader.GetOrdinal("file")),
+            Formula = reader.IsDBNull(reader.GetOrdinal("formula")) ? "N/A" : reader.GetString("formula"),
+            Name = reader.IsDBNull(reader.GetOrdinal("commonname")) ? "Mineral" : reader.GetString("commonname"),
+            Year = reader.IsDBNull(reader.GetOrdinal("year")) ? null : reader.GetInt32("year"),
+            
+            A = reader.IsDBNull(reader.GetOrdinal("a")) ? null : Convert.ToDouble(reader.GetValue(reader.GetOrdinal("a"))),
+            B = reader.IsDBNull(reader.GetOrdinal("b")) ? null : Convert.ToDouble(reader.GetValue(reader.GetOrdinal("b"))),
+            C = reader.IsDBNull(reader.GetOrdinal("c")) ? null : Convert.ToDouble(reader.GetValue(reader.GetOrdinal("c"))),
+            Alpha = reader.IsDBNull(reader.GetOrdinal("alpha")) ? null : Convert.ToDouble(reader.GetValue(reader.GetOrdinal("alpha"))),
+            Beta = reader.IsDBNull(reader.GetOrdinal("beta")) ? null : Convert.ToDouble(reader.GetValue(reader.GetOrdinal("beta"))),
+            Gamma = reader.IsDBNull(reader.GetOrdinal("gamma")) ? null : Convert.ToDouble(reader.GetValue(reader.GetOrdinal("gamma")))
+        });
     }
-    return results;
+
+    return results; 
 }
 }
