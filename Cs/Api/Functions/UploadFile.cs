92d036bbc2dccf866982393a55eb59ce977a9f4d
FrankLovelace <alexandrogallegos12@gmail.com>
2026-03-12 14:10:03 -0600

    we modify loading function in cs the new function should receive the csv from vue then do a http call to python to clean K factor

--- Cs/Api/Functions/UploadFile.cs
+++ Cs/Api/Functions/UploadFile.cs
@@ -2,27 +2,67 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Api.Parsers;
+using Api.Models;
+using System.Text.Json;
+using System.Net.Http.Json;
 
 namespace Api.Functions;
 
 public class UploadFile
 {
-    private readonly CsvParser _parser; 
+    private readonly CsvParser _parser;
+    private readonly HttpClient _httpClient;
 
     public UploadFile(CsvParser parser)
     {
         _parser = parser;
+        _httpClient = new HttpClient();
     }
 
     [Function("UploadFile")]
     public async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "upload")] HttpRequest req)
     {
-        var file = req.Form.Files.GetFile("file");
-        if (file == null) return new BadRequestObjectResult("No se subió ningún archivo.");
+        var form = await req.ReadFormAsync();
+        var file = form.Files.GetFile("file");
+        
+        if (file == null) 
+            return new BadRequestObjectResult("No se subió ningún archivo.");
+
+        var anode = form.ContainsKey("anode") ? form["anode"].ToString() : "Cu";
+        var isStripped = form.ContainsKey("isStripped") && form["isStripped"] == "true";
 
         using var stream = file.OpenReadStream();
         var data = _parser.Parse(stream);
+        data.Anode = anode;
+
+        if (!isStripped)
+        {
+            try
+            {
+                var pyRequest = new {
+                    two_theta = data.TwoTheta,
+                    intensity = data.Intensity,
+                    anode = anode
+                };
+
+                var response = await _httpClient.PostAsJsonAsync("http://localhost:8000/api/math/strip-ka2", pyRequest);
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    var pyResult = await response.Content.ReadFromJsonAsync<JsonElement>();
+                    
+                    var cleanedArray = pyResult.GetProperty("cleaned_intensity").EnumerateArray()
+                                               .Select(x => x.GetDouble()).ToArray();
+                    
+                    data.Intensity = cleanedArray; 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error conectando al motor Python: {ex.Message}");
+            }
+        }
 
         return new OkObjectResult(data);
     }
