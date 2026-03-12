fde27dee00911d06013b14c6bfc6118c95ea4c3e
FrankLovelace <alexandrogallegos12@gmail.com>
2026-03-12 10:31:16 -0600

    we add a model Cod Result to send crystalographic data to vue

---
+++ Cs/Api/Models/CodResult.cs
@@ -0,0 +1,15 @@
+namespace Api.Models;
+
+public class CodResult
+{
+    public int Id { get; set; }
+    public string Formula { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int? Year { get; set; }
+    public double? A { get; set; }
+    public double? B { get; set; }
+    public double? C { get; set; }
+    public double? Alpha { get; set; }
+    public double? Beta { get; set; }
+    public double? Gamma { get; set; }
+}
