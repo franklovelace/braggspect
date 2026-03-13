63b07ac7efb6843ba49837a946d9794505bfc3e7
FrankLovelace <alexandrogallegos12@gmail.com>
2026-03-13 16:39:02 -0600

    we modify the upload logic so it always call python if the data is stripped and even if is not so it can use hanawalt script

--- Cs/Api/Functions/UploadFile.cs
+++ Cs/Api/Functions/UploadFile.cs
@@ -20,50 +20,52 @@ public class UploadFile
     }
 
     [Function("UploadFile")]
-    public async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "upload")] HttpRequest req)
+public async Task<IActionResult> Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "upload")] HttpRequest req)
+{
+    var form = await req.ReadFormAsync();
+    var file = form.Files.GetFile("file");
+    
+    if (file == null) 
+        return new BadRequestObjectResult("No se subió ningún archivo.");
+
+    var anode = form.ContainsKey("anode") ? form["anode"].ToString() : "Cu";
+    var isStripped = form.ContainsKey("isStripped") && form["isStripped"] == "true";
+
+    using var stream = file.OpenReadStream();
+    var data = _parser.Parse(stream);
+    data.Anode = anode;
+
+    try
     {
-        var form = await req.ReadFormAsync();
-        var file = form.Files.GetFile("file");
+        var pyRequest = new {
+            two_theta = data.TwoTheta,
+            intensity = data.Intensity,
+            anode = anode,
+            is_stripped = isStripped 
+        };
+
+        var response = await _httpClient.PostAsJsonAsync("http://localhost:8000/api/math/process", pyRequest);
         
-        if (file == null) 
-            return new BadRequestObjectResult("No se subió ningún archivo.");
-
-        var anode = form.ContainsKey("anode") ? form["anode"].ToString() : "Cu";
-        var isStripped = form.ContainsKey("isStripped") && form["isStripped"] == "true";
-
-        using var stream = file.OpenReadStream();
-        var data = _parser.Parse(stream);
-        data.Anode = anode;
-
-        if (!isStripped)
+        if (response.IsSuccessStatusCode)
         {
-            try
+            var pyResult = await response.Content.ReadFromJsonAsync<JsonElement>();
+            
+            var cleanedArray = pyResult.GetProperty("cleaned_intensity").EnumerateArray().Select(x => x.GetDouble()).ToArray();
+            data.Intensity = cleanedArray; 
+            
+            if (pyResult.TryGetProperty("top_peaks", out JsonElement peaksElement))
             {
-                var pyRequest = new {
-                    two_theta = data.TwoTheta,
-                    intensity = data.Intensity,
-                    anode = anode
-                };
-
-                var response = await _httpClient.PostAsJsonAsync("http://localhost:8000/api/math/strip-ka2", pyRequest);
-                
-                if (response.IsSuccessStatusCode)
-                {
-                    var pyResult = await response.Content.ReadFromJsonAsync<JsonElement>();
-                    
-                    var cleanedArray = pyResult.GetProperty("cleaned_intensity").EnumerateArray()
-                                               .Select(x => x.GetDouble()).ToArray();
-                    
-                    data.Intensity = cleanedArray; 
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error conectando al motor Python: {ex.Message}");
+                data.TopPeaks = JsonSerializer.Deserialize<List<PeakData>>(peaksElement.GetRawText(), 
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? new List<PeakData>();
             }
         }
-
-        return new OkObjectResult(data);
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error conectando al motor Python: {ex.Message}");
+    }
+
+    return new OkObjectResult(data);
+}
 }
