ccd6301a9c17c87fa3b73a09c108d8c9d735a49a
FrankLovelace <alexandrogallegos12@gmail.com>
2026-03-13 16:33:54 -0600

    we create PeakData class and we added the peak list to my main class

--- Cs/Api/Models/DrxDisplayData.cs
+++ Cs/Api/Models/DrxDisplayData.cs
@@ -1,9 +1,16 @@
 namespace Api.Models; 
+public class PeakData
+{
+    public double TwoTheta { get; set; }
+    public double DSpacing { get; set; }
+    public double Intensity { get; set; }
+}
 public class DrxDisplayData
 {
     public string SampleName { get; set; } = string.Empty;
     public double[] TwoTheta { get; set; } = Array.Empty<double>();
     public double[] Intensity { get; set; } = Array.Empty<double>();
     public string Anode { get; set; } = "Cu";
-    public double Wavelength { get; set; } = 1.5406;
+    
+    public List<PeakData> TopPeaks { get; set; } = new List<PeakData>();
 }
