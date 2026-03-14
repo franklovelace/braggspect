862702c6464546fe45a280e48d960f6a9c5bcbee
FrankLovelace <alexandrogallegos12@gmail.com>
2026-03-13 20:33:44 -0600

    finally we register the new page in the router

--- web/src/router.ts
+++ web/src/router.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Workflow from './views/Workflow.vue';
 import CodSearch from './views/CodSearch.vue';
 import Analysis from './views/Analysis.vue';
+import Candidates from './views/Candidates.vue';
 
 const routes = [
   { path: '/', component: Workflow },
   { path: '/search', component: CodSearch },
-  { path: '/analysis', component: Analysis }
+  { path: '/analysis', component: Analysis },
+  { path: '/candidates', component: Candidates }
 ];
 
 export const router = createRouter({
