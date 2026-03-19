5fff5d2fbdb2c3b98e8977f604f6d0bb2adca1ac
FrankLovelace <alexandrogallegos12@gmail.com>
2026-03-19 10:03:07 -0600

    cod resultDTO

--- Cs/Api/Models/CodResult.cs
+++ Cs/Api/Models/CodResult.cs
@@ -5,6 +5,7 @@ public class CodResult
     public int Id { get; set; }
     public string Formula { get; set; } = "";
     public string Name { get; set; } = "";
+    public double MatchScore { get; set; }
     public int? Year { get; set; }
     public double? A { get; set; }
     public double? B { get; set; }
@@ -12,4 +13,5 @@ public class CodResult
     public double? Alpha { get; set; }
     public double? Beta { get; set; }
     public double? Gamma { get; set; }
+    public List<PeakData> TheoreticalPeaks { get; set; } = new List<PeakData>();
 }
